76139d0b429f418a27659f82109a026c168a74da
안정후 <austinan@naver.com>
2023-06-19 10:25:16 +0900

    chore: initial project

---
+++ src/main/java/com/dreamroad/dreamroad/DreamroadApplication.java
@@ -0,0 +1,13 @@
+package com.dreamroad.dreamroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamroadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamroadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamroad/dreamroad/DreamroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamroad.dreamroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamroadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
